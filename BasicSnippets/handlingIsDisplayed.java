8f08d59e8908317bef9ea6d78e9e226e21c2ff72
P S Shrijith <p.shrijith@talview.com>
2022-10-28 14:41:03 +0530

    handleIsDisplayed
    The small snippet allows you to handle isDisplayed method regardless of element is present or not

---
+++ BasicSnippets/handlingIsDisplayed.java
@@ -0,0 +1,11 @@
+//This snippet will help in handling is displayed in a if else condition
+
+
+public void compeletion() throws InterruptedException {
+		try {
+		//if condition goes here
+		}
+		catch(Exception e) {
+		//else condition goes here
+		}
+	}
