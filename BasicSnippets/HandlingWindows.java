e87643a2703248a2a43570284aad65db8c61949d
P S Shrijith <p.shrijith@talview.com>
2022-10-28 14:41:49 +0530

    handlingWindows
    Handle Tabs

---
+++ BasicSnippets/HandlingWindows.java
@@ -0,0 +1,27 @@
+package practiceexamples;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HandlingWindows {
+	public static void main(String[] args) throws InterruptedException {
+	
+		/**
+		*
+		*The snippet will help to handle tabs in selenium
+		*
+		*/
+		
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.salesforce.com/in/form/signup/freetrial-sales/?d=70130000000Enyk");
+		String window1 = driver.getWindowHandle();
+		driver.executeScript("window.open('https://www.google.com','_blank');");
+		String window2 = driver.getWindowHandle();
+		Thread.sleep(2000);
+		driver.switchTo().window(window1);
+	}
+
+}
