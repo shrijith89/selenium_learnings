ffbf1570143e9e3daef6527d71744193e962f443
P S Shrijith <p.shrijith@talview.com>
2022-10-28 14:42:13 +0530

    hoveringOverWebelement

---
+++ BasicSnippets/HoveringElement.java
@@ -0,0 +1,26 @@
+package practiceexamples;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HoveringElement {
+	public static void main(String[] args) {
+		
+		/**
+		* Hover over a webelement using action class
+		*
+		*/
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.ebay.com/");
+		Actions action = new Actions(driver);
+		WebElement target = driver.findElement(By.xpath("(//a[@href='https://www.ebay.com/b/Electronics/bn_7000259124'])[2]"));
+		action.moveToElement(target).perform();
+	}
+
+}
