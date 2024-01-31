dabe1f4dcc2bf35d25eb788dd270ca117b5d4290
P S Shrijith <shrijithps89@gmail.com>
2024-01-31 23:45:01 +0530

    chromeChangesafternewVersion

---
+++ BasicSnippets/ChromeChange.java
@@ -0,0 +1,15 @@
+package com.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ChromeChange {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\PSShrijith\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		ChromeOptions co = new ChromeOptions();
+		co.setBinary("C:\\Users\\PSShrijith\\Downloads\\chrome-win64\\chrome-win64\\chrome.exe");
+		WebDriver driver = new ChromeDriver(co);
+		driver.get("https://www.google.com");
+	}
+}
