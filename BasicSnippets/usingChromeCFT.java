352e5fbdce44bf506811f298870d9f78ce2e9fdd
P S Shrijith <shrijithps89@gmail.com>
2024-02-01 16:38:19 +0530

    usingChromeCFT

---
+++ BasicSnippets/usingChromeCFT.java
@@ -0,0 +1,15 @@
+package com.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class usingChromeCFT {
+	public static void main(String[] args) {
+		ChromeOptions ch = new ChromeOptions();
+		ch.setBrowserVersion("116");
+		WebDriver driver = new ChromeDriver(ch);
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+	}
+}
