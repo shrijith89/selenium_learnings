263996aeb79c557eee83dc4847eaa63dfe4566a3
P S Shrijith <p.shrijith@talview.com>
2024-02-10 20:36:01 +0530

    Update ChromeChange.java

--- BasicSnippets/ChromeChange.java
+++ BasicSnippets/ChromeChange.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ChromeChange {
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\PSShrijith\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\PSShrijith\\Documents\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 		ChromeOptions co = new ChromeOptions();
 		co.setBinary("C:\\Users\\PSShrijith\\Downloads\\chrome-win64\\chrome-win64\\chrome.exe");
 		WebDriver driver = new ChromeDriver(co);
